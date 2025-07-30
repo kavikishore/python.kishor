849f568b880bdcab1de15b4085dd816f96332438
kavikishore <kavikishore779@gmail.com>
2025-07-30 16:08:38 +0530

    Add files via upload

---
+++ kishor2.py
@@ -0,0 +1,15 @@
+print("takshashila university")
+print("ongur,tindivanam villupuram district,tamilnadu")
+print("--------------------")
+print("STUDENT MARK LIST")
+print("--------------------")
+strl=input("enter the regester NO:")
+str2=input("enter the name:")
+ml =int(input("python mark:"))
+m2 =int(input("DBMS mark:"))
+m3 =int(input("account mark:"))
+total = ml + m2 +m3
+print("total mark:", total)
+avg=total/3
+print("average mark:",avg)      
+      
---
+++ kishor4.py
@@ -0,0 +1,13 @@
+print("kishor supermarket")
+print("NO.184,mariyaman kovil street,melpettai.")
+print("**************")
+print("Bill recipet")
+print("**************")
+sl=input("Enter the serial number:")
+pr=input("Enter the praticulers:")
+rate =int(input("Enter the rate:"))
+qty =int(input("Enter the ouality:"))
+total =rate * qty
+print("Total Amount :",total)
+GST =total * 10 / 100
+print("GST (10%):",GST)
