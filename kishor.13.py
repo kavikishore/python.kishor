f47479624d36246e1ed879d2f9ef3019c0799d47
kavikishore <kavikishore779@gmail.com>
2025-11-03 10:52:04 +0530

    Add files via upload

--- kishor.13.py
+++ kishor.13.py
@@ -8,16 +8,16 @@ print("      Bill reciept")
 
 print(".........................")
 
-no=int (input("Enter the item no:"))
-prt=input("Enter the particular:")
+no=int(input("Enter the item no:"))
+name=input("Enter the particular:")
 rate=int(input ("Enter the rate:"))
-qty=int (input("Enter the quantity:"))
+qty=int(input("Enter the quantity:"))
 total=rate*qty
 print("Total amount is rupees", total)
 if (total>=100000):
     gst=total*10/100
 elif (total>=50000):
-     gst=toatl*5/100
+     gst=total*5/100
 elif (total>=30000):
     gst=total *3/100
 elif (total>=10000):
@@ -25,5 +25,5 @@ elif (total>=10000):
 else:
     gst=total*1/100
     print("Gst (gat (good & service tax",gst)
-    amount=total+gst
-    print("Amount to be paid", amount)
+    amt=total+gst
+    print("Amount to be paid",amt)
