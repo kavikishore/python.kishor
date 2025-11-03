251cf73f20ef36adec525bcccb64d15ad668b465
kavikishore <kavikishore779@gmail.com>
2025-11-03 11:00:35 +0530

    Enhance output with number ranges and sequences
    Added various print statements to display ranges and sequences.

--- kishor.14.py
+++ kishor.14.py
@@ -1,7 +1,13 @@
-print("factorial program")
-print("-----------------")
-n=int(input("enter the number:"))
-fact=1
-for i in range (1,n+1):
-    fact=fact*n
-    print("fact:",fact)
+print("staring (0,9) ending number")
+for i in range(10):
+    print(i)
+print("starting (50,100) ending number")
+for j in range(50,101):
+    print(j)
+print("even number:")
+for s in range(100,127,2):
+    print(s)
+print("10 to 1 forwad")
+for u in range(10,0,-1):
+    print(u)
+    
