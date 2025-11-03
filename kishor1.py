40e49ff7929c64f6ee8bd25abec67b96315cd1af
kavikishore <kavikishore779@gmail.com>
2025-11-03 10:55:40 +0530

    Delete kishor1.py

--- kishor1.py
+++
@@ -1,9 +0,0 @@
-print("Arithmetic operations")
-print("*********************")
-a =int(input("enter the first number :"))
-b =int(input("enter the second number:"))
-print("*Result*")
-c=a+b
-print("ADD:",c)
-d=a-b
-print("SUB:",d)
